5b0da7038cf270f15f17d3a0d53f2459a51bda94
Adam Rackis <adamrackis@hotmail.com>
2022-12-04 16:52:41 -0600

    Bug repro

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from "svelte";
+  import SimpleForm from "./SimpleForm.svelte";
+
+  onMount(() => {
+    console.log("MOUNT");
+
+    const portal = document.createElement("div");
+    document.body.appendChild(portal);
+    new SimpleForm({ target: portal });
+  });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+This one works: <SimpleForm />
+
+This one does not, and causes a full re-render
---
+++ src/routes/SimpleForm.svelte
@@ -0,0 +1,3 @@
+<form>
+  <input name="search" />
+</form>
